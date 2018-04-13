c0cb9f4083b9758f880535abb71897291fdc2c00
황보성훈 <HBSH@hwangboseonghuns-MacBook-Pro.local>
2018-04-14 00:26:23 +0900

    Blockchain

---
+++ Blockchain_implement/noobchain/src/noobchain/StringUtil.java
@@ -0,0 +1,98 @@
+package noobchain;
+
+import java.security.Key;
+import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.util.ArrayList;
+import java.util.Base64;
+
+
+public class StringUtil {
+	//Applies SHA-256 to a string and returns the result.
+	public static String applySha256(String input) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			//Applies sha256 to our input.
+			byte[] hash = digest.digest(input.getBytes("UTF-8"));
+			StringBuffer hexString = new StringBuffer(); //This will contain hash as hexidecimal
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if (hex.length() == 1) {
+					hexString.append('0');					
+				}
+				hexString.append(hex);
+			}
+			return hexString.toString();
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	//Applies ECDSA Signature and returns the result (as bytes).
+	//private key를 이용해서 거래 정보를 암호 -> signature를 만듦.
+	public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
+		Signature dsa;
+		byte[] output = new byte[0];
+		try {
+			dsa = Signature.getInstance("ECDSA", "BC");
+			dsa.initSign(privateKey);
+			byte[] strByte = input.getBytes();
+			dsa.update(strByte);
+			byte[] realSig = dsa.sign();
+			output = realSig;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return output;
+	}
+	
+	//Verifies a String signature
+	//누구나 public key를 가지고 있음 -> public key와 거래 정보를 이용해서 signature를 만듦 -> 만든 signature가 원래의 signature와 동일하면 정상 거래.
+	public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
+		try {
+			Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+			ecdsaVerify.initVerify(publicKey);
+			ecdsaVerify.update(data.getBytes());
+			return ecdsaVerify.verify(signature);			
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static String getStringFromKey(Key key) {
+		return Base64.getEncoder().encodeToString(key.getEncoded());
+	}
+	
+	//Tacks in array of transactions and returns a merkle root.
+	public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+		int count = transactions.size();
+		ArrayList<String> previousTreeLayer = new ArrayList<String>();
+		for (Transaction transaction : transactions) {
+			previousTreeLayer.add(transaction.transactionId);
+		}
+		ArrayList<String> treeLayer = previousTreeLayer;
+		while(count > 1) {
+			treeLayer = new ArrayList<String>();
+			for (int i = 1; i <previousTreeLayer.size(); i++) {
+				treeLayer.add(applySha256(previousTreeLayer.get(i - 1)));
+			}
+			count = treeLayer.size();
+			previousTreeLayer = treeLayer;
+		}
+		String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+		return merkleRoot;
+		}
+
+	public static String getDifficultyString(int difficulty) {
+		
+		String difficultyString = new String(new char[difficulty]).replace('\0', '0');
+		
+		return difficultyString;
+	}
+	
+	
+	
+	
+}
